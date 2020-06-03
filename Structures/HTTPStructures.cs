b31848b33b379020e48cc4b7e8a570dc50fc4494
jay-tux <sys.jonas@yahoo.com>
2020-06-03 10:38:46 +0200

    Added action to download web url

--- Structures/HTTPStructures.cs
+++ Structures/HTTPStructures.cs
@@ -5,6 +5,7 @@ using Jay.VTS.Execution;
 using Jay.Xtend;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace Jay.VTS.Structures
 {
@@ -26,6 +27,38 @@ namespace Jay.VTS.Structures
                             throw VTSException.TypeException(CoreStructures.VTSString, args[0].Class, frame);
                         caller.Fields["url"] = args[0];
                         return caller;
+                    }),
+                    ["getString"] = ((caller, args, frame) => {
+                        if(args.Count != 1)
+                            throw VTSException.ArgCountException("WebClient", "getString", 1, (uint)args.Count, frame);
+                        if(args[0].Class != CoreStructures.VTSString) {
+                            throw VTSException.TypeException(CoreStructures.VTSString, args[0].Class, frame);
+                        }
+
+                        try {
+                            string url = (string)((VTSVariable)caller.Fields["url"]).Fields["value"];
+                            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(url);
+                            Request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                            Request.Proxy = null;
+                            Request.Method = (string)args[0].Fields["value"];
+                            using (WebResponse Response = Request.GetResponse())
+                            {
+                                using (StreamReader Reader = new StreamReader(Response.GetResponseStream()))
+                                {
+                                    return new VTSVariable() {
+                                        Class = CoreStructures.VTSString, Mutable = false,
+                                        Fields = new Dictionary<string, object>() {
+                                            ["value"] = Reader.ReadToEnd()
+                                        }
+                                    };
+                                }
+                            }
+                        }
+                        catch(Exception e) {
+                            throw new VTSException("WebError", frame, 
+                                "Failed downloading url " + (string)((VTSVariable)caller.Fields["url"]).Fields["value"] + 
+                                " via " + (string)args[0].Fields["value"], null);
+                        }
                     })
                 }
             };
